00171f59a3e7e377d1f8a6ceb3190fa880c42dbd
beatface <emma.beaton@gmail.com>
2016-01-25 15:28:26 -0600

    added template strings tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -4,5 +4,29 @@ describe('template strings', function() {
   it('should be a string', function(){
     var string = `abc`;
     expect(string).to.be.a('string');
-  })
+  });
+
+  it('can evaluate variables', function() {
+    var bar = "baz";
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+
+  it('can excecute a function', function() {
+    var string = `the date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal('the date is: ' + date);
+  });
+
+  it('can evaulate an expression', function() {
+    var string = `num is ${1+1}`;
+    expect(string).to.equal('num is 2');
+  });
+
+  it('can evaluate multi-line strings', function() {
+    var string = `hello
+world`;
+    expect(string).to.equal('hello\nworld');
+  });
 })
